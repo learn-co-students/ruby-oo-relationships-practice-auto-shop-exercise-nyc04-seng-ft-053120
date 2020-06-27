e2bf510db26fa282a5e3c37ddfbeb45b4117f8f9
AlveeM <alvee.akand@outlook.com>
2020-06-27 19:19:02 -0400

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,26 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification
+
+  @@all = []
 
   def initialize(make, model, classification)
     @make = make
     @model = model
     @classification = classification
+    @@all << self 
+  end
+
+  def self.classifications
+    self.all.map { |car| car.classification }
+  end
+
+  def self.find_mechanics(classification)
+    Mechanic.all.select { |mechanic| mechanic.specialty == classification }
+  end
+
+  def self.all 
+    @@all
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,13 @@ def reload
   load 'config/environment.rb'
 end
 
+c1 = Car.new("Ferrari", "458", "exotic")
+c2 = Car.new("Toyota", "Splinter", "antique")
+c3 = Car.new("Volkswagen", "Beetle", "clunker")
+
+co1 = CarOwner.new("Mark")
+co2 = CarOwner.new("Jane")
+
+m1 = Mechanic.new("Hamil", "exotic", c1, co1)
+
 binding.pry
